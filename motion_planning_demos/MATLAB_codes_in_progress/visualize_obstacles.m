420769d563314131db61c1987892eb38435adb5d
ronen-g-aniti <ronen.g.aniti@gmail.com>
2024-05-26 16:02:41 -0400

    Adding telemetry data processor C# project

--- motion_planning_demos/MATLAB_codes_in_progress/visualize_obstacles.m
+++
@@ -1,4 +0,0 @@
-% I will create a MATLAB program to parse the obstacle data and then visualize the obstacles on a 3D plot
-% The script will contain classes and functions similar to those of environment_data.py
-% The basic purpose of creating this script will be to brush up on my MATLAB programming skills
-
---
+++ telemetry_data_processor/telemetry_data_processor/telemetry_data_processor/Form1.cs
@@ -0,0 +1,10 @@
+namespace telemetry_data_processor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
